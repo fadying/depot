4f991c71953c341c163211e81110578ab2ee1d91
fadying <oys0818@163.com>
2014-01-03 13:05:34 +0800

    locale filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :authorize
+  before_filter :authorize, :set_i18n_locale_from_params
   protect_from_forgery with: :exception
   
   private
@@ -36,5 +36,21 @@ class ApplicationController < ActionController::Base
         user && user.authenticate(password)
       end
     end
+  end
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = 
+          "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,17 @@ Depot::Application.routes.draw do
   end
 
   resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get "store/index"
   resources :products do
     member do
       get :who_bought
     end
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root to: 'store#index', :as => 'store'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -27,7 +26,6 @@ Depot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root :to => 'store#index', :as => 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
